c8371354054b7407f98872e0e40b87807c6325bc
beasticle <julian.heidt@gmail.com>
2022-01-12 18:43:40 -0500

    updates map coords

--- src/main/java/frc/libraries/Map1038.java
+++ src/main/java/frc/libraries/Map1038.java
@@ -1,28 +1,56 @@
 package frc.libraries;
 
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-
-import frc.libraries.CANSpark1038;
+import frc.libraries.Limelight1038;
+import frc.libraries.DriveTrain1038;
+import frc.libraries.Gyro1038;
 
 public class Map1038 implements Subsystem {
-    public enum startingCoords {pos1X, pos1Y, pos2X, pos2Y};
+    private Limelight1038 limelight = Limelight1038.getInstance();
+    private Gyro1038 gyro = Gyro1038.getInstance();
+    private DriveTrain1038 drivetrain = DriveTrain1038.getInstance();
+
+    private double gryoPos = gyro.getAngle();
+
+    public double limelightZ = limelight.getYOffset();
+    public int pos1X = 1;
+    public int pos1Y = 1;
     final double hubHeight = 8.8; //8 feet 8 inches
-    
-    
+    public double actualZ = hubHeight - limelightZ;
 
+    private double encoderChange = 0;
 
-    public void updateCoords() {
-        
+    
+    public void resetSensor() {
+        gyro.reset();
+        drivetrain.resetEncoders();
+    }
+    
+    public distanceChange() {
+        gryoPos = gryo.getAngle;
+        if (0 <= gryoPos || gryoPos < 90) {
+             xChange = encoderChange * sin(gryoPos);
+             yChange = -(encoderChange * cos(gryoPos));
+        }
+        else if (90 <= gryoPos || gryoPos < 180) {
+            xChange = -(encoderChange * cos(gryoPos - 90));
+            yChange = -(encoderChange * sin(gryoPos - 90));
+        }
+        else if (180 <= gryoPos || gryoPos < 270) {
+            xChange = -(encoderChange * sin(gyroPos - 180));
+            yChange = encoderChange * cos(gyroPos - 180);
+        }
+        else {
+            xChange = encoderChange * cos(gyroPos - 270);
+            yChange = encoderChange * sin(gyroPos - 270);
+        }
+
+        pos1X += xChange;
+        pos1Y += yChange;
     }
 
-    public void turretAngle() {
-        
+    public void turretAngle(double currentAngle) {
+        atan(pos1X/pos1Y); //absolute angle that the turret needs. This is only correct if the robot hasn't turned. 
     }
 
     public void resetCoords() {
