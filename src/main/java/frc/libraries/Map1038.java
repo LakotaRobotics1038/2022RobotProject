6c478c1f0e6169a47fc1af35ae73b8e385a2a007
Julian Heidt <heidtje@lr1038.local>
2022-01-12 17:18:15 -0500

    map code skeleton

--- src/main/java/frc/libraries/Map1038.java
+++ src/main/java/frc/libraries/Map1038.java
@@ -11,5 +11,24 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import frc.libraries.CANSpark1038;
 
 public class Map1038 implements Subsystem {
+    public enum startingCoords {pos1X, pos1Y, pos2X, pos2Y};
+    final double hubHeight = 8.8; //8 feet 8 inches
     
+    
+
+
+    public void updateCoords() {
+        
+    }
+
+    public void turretAngle() {
+        
+    }
+
+    public void resetCoords() {
+
+    }
+
+
+
 }
