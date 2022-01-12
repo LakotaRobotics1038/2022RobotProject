8b33afe0e543b277e48b7f6f0162b3ea95dea70b
Julian Heidt <heidtje@lr1038.local>
2022-01-11 19:24:34 -0500

    barebones code, ready to add on

---
+++ src/main/java/frc/libraries/Map1038.java
@@ -0,0 +1,15 @@
+package frc.libraries;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+import frc.libraries.CANSpark1038;
+
+public class Map1038 implements Subsystem {
+    
+}
