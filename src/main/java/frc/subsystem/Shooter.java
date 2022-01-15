69aa6f6bf365d5302c08475e0bd9e470733da538
Caleb Kingan <ckingan@lr1038.local>
2022-01-15 13:23:54 -0500

    All Skeleton classes except limelight and Comms are donegit statusgit status

--- src/main/java/frc/subsystem/Shooter.java
+++ src/main/java/frc/subsystem/Shooter.java
@@ -3,21 +3,35 @@ package frc.subsystem;
 import frc.libraries.TalonSRX1038;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.libraries.Limelight1038;
+import frc.libraries.TalonFX1038;
 
 public class Shooter implements Subsystem {
+    private static Shooter shooter;
+    public static Shooter getInstance() {
+        if (shooter == null) {
+          System.out.println("Creating a new Shooter");
+          shooter = new Shooter();
+        }
+        return shooter;
+      }
     private final int SHOOTER_MOTOR_PORT1 = 0;
     private final int SHOOTER_MOTOR_PORT2 = 0;
     private final int HOOD_MOTOR_PORT = 0;
     private final int TURRET_MOTOR_PORT = 0;
     private final int CONVERSION_NUM = 0;
 
-    public TalonSRX1038 shooterMotor1 = new TalonSRX1038(SHOOTER_MOTOR_PORT1);
-    public TalonSRX1038 shooterMotor2 = new TalonSRX1038(SHOOTER_MOTOR_PORT2);
-    public TalonSRX1038 hoodMotor = new TalonSRX1038(HOOD_MOTOR_PORT);
-    public TalonSRX1038 turretMotor = new TalonSRX1038(TURRET_MOTOR_PORT);
+    public TalonFX1038 shooterMotor1 = new TalonFX1038(SHOOTER_MOTOR_PORT1);
+    public TalonFX1038 shooterMotor2 = new TalonFX1038(SHOOTER_MOTOR_PORT2);
+    public TalonFX1038 hoodMotor = new TalonFX1038(HOOD_MOTOR_PORT);
+    public TalonFX1038 turretMotor = new TalonFX1038(TURRET_MOTOR_PORT);
 
     public void turretRotate() {
 
     }
-
+    public double getTurretEncoder() {
+        return turretMotor.getPosition();
+    }
+    // public boolean getHardStop() {
+    //     return hardStop.get();
+    // }
 }
