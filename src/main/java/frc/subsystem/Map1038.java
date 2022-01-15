0743f9f48b36c7b7bb83fc75bd89ea6ef5119370
Caleb Kingan <ckingan@lr1038.local>
2022-01-15 10:30:01 -0500

    Julian fixed issues, my laptop didn't give me errors

--- src/main/java/frc/subsystem/Map1038.java
+++ src/main/java/frc/subsystem/Map1038.java
@@ -4,17 +4,18 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.libraries.Limelight1038;
 import frc.libraries.DriveTrain1038;
 import frc.libraries.Gyro1038;
+import java.lang.Math;
 
 public class Map1038 implements Subsystem {
     private Limelight1038 limelight = Limelight1038.getInstance();
     private Gyro1038 gyro = Gyro1038.getInstance();
     private DriveTrain1038 drivetrain = DriveTrain1038.getInstance();
 
-    private double gryoPos = gyro.getAngle();
+    private double gyroPos = gyro.getAngle();
 
     public double limelightZ = limelight.getYOffset();
-    public int pos1X = 1;
-    public int pos1Y = 1;
+    public double pos1X = 1;
+    public double pos1Y = 1;
     final double hubHeight = 8.8; //8 feet 8 inches
     public double actualZ = hubHeight - limelightZ;
 
@@ -27,45 +28,42 @@ public class Map1038 implements Subsystem {
     }
     
     public void distanceChange() {
-        gryoPos = gryo.getAngle;
-        if (0 <= gryoPos || gryoPos < 90) {
-             xChange = encoderChange * sin(gryoPos);
-             yChange = -(encoderChange * cos(gryoPos));
+        double gyroPos = gyro.getAngle();
+        double xChange = 0;
+        double yChange = 0;
+        if (0 <= gyroPos || gyroPos < 90) {
+             xChange = encoderChange * Math.sin(gyroPos);
+             yChange = -(encoderChange * Math.cos(gyroPos));
         }
-        else if (90 <= gryoPos || gryoPos < 180) {
-            xChange = -(encoderChange * cos(gryoPos - 90));
-            yChange = -(encoderChange * sin(gryoPos - 90));
+        else if (90 <= gyroPos || gyroPos < 180) {
+            xChange = -(encoderChange * Math.cos(gyroPos - 90));
+            yChange = -(encoderChange * Math.sin(gyroPos - 90));
         }
-        else if (180 <= gryoPos || gryoPos < 270) {
-            xChange = -(encoderChange * sin(gyroPos - 180));
-            yChange = encoderChange * cos(gyroPos - 180);
+        else if (180 <= gyroPos || gyroPos < 270) {
+            xChange = -(encoderChange * Math.sin(gyroPos - 180));
+            yChange = encoderChange * Math.cos(gyroPos - 180);
         }
         else {
-            xChange = encoderChange * cos(gyroPos - 270);
-            yChange = encoderChange * sin(gyroPos - 270);
+            xChange = encoderChange * Math.cos(gyroPos - 270);
+            yChange = encoderChange * Math.sin(gyroPos - 270);
         }
 
         pos1X += xChange;
         pos1Y += yChange;
     }
 
-    public void turretAngle(double currentTurretPos) {
+    public double turretAngle(double currentTurretPos) {
         gyroPos = gyro.getAngle();
-        targetAngle = atan(pos1X/pos1Y) - gyroPos;
-        moveAngle = currentTurretPos - targetAngle; //turngle
+        double targetAngle = Math.atan(pos1X/pos1Y) - gyroPos;
+        return currentTurretPos - targetAngle; //turngle
     }
 
-    public void resetCoords(double limelightAngle) {
+    public void resetCoords() {
+        double a = limelight.getXOffset();
+        double b = limelight.getYOffset();
         a *= pos1X/Math.abs(pos1X);
-        b *= pos1Y/Math.abs(pos1Y);
+        b *= (pos1Y/Math.abs(pos1Y));
         pos1X = a;
         pos1Y = b;
     }
-
-    public double readTurretAngle() {
-        return targetAngle;
-    }
-
-
-
 }
--- src/main/java/frc/subsystem/Shooter.java
+++ src/main/java/frc/subsystem/Shooter.java
@@ -1,6 +1,7 @@
 package frc.subsystem;
 
 import frc.libraries.TalonSRX1038;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.libraries.Limelight1038;
 
 public class Shooter implements Subsystem {
@@ -11,10 +12,10 @@ public class Shooter implements Subsystem {
     private final int TURRET_MOTOR_PORT = 0;
     private final int CONVERSION_NUM = 0;
 
-    public TalonSRX1038 shooterMotor1 = new TalonSRX1038(shooterMotorPort1);
-    public TalonSRX1038 shooterMotor2 = new TalonSRX1038(shooterMotorPort2);
-    public TalonSRX1038 hoodMotor = new TalonSRX1038(hoodMotorPort);
-    public TalonSRX1038 turretMotor = new TalonSRX1038(turretMotorPort);
+    public TalonSRX1038 shooterMotor1 = new TalonSRX1038(SHOOTER_MOTOR_PORT1);
+    public TalonSRX1038 shooterMotor2 = new TalonSRX1038(SHOOTER_MOTOR_PORT2);
+    public TalonSRX1038 hoodMotor = new TalonSRX1038(HOOD_MOTOR_PORT);
+    public TalonSRX1038 turretMotor = new TalonSRX1038(TURRET_MOTOR_PORT);
 
     public void turretRotate() {
 
