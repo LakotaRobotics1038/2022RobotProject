e381b2f1698e68a1219e07cc238cbb0ae405c3de
DuckierTrain <crkingan@gmail.com>
2022-01-22 14:40:45 -0500

    New Talon feature

--- src/main/java/frc/subsystem/Endgame.java
+++ src/main/java/frc/subsystem/Endgame.java
@@ -18,8 +18,7 @@ public class Endgame {
     private final int ROTATOR_PORT = 0;
 
     //Initalize objects
-    final CANSpark1038 rotator = new CANSpark1038(ELEVATOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
-    final CANEncoder rotateEncoder = rotator.getEncoder();
+    final TalonFX1038 rotator = new TalonFX1038(ROTATOR_PORT);
     final TalonFX1038 elevator = new TalonFX1038(ROTATOR_PORT);
 
     //Encoders for the Motors
@@ -27,7 +26,7 @@ public class Endgame {
         return elevator.getPosition();
     }
     public double getRotatorEncodePosition() {
-        return rotateEncoder.getPosition(); //might need some extra weird math
+        return rotator.getPosition(); //might need some extra weird math
     }
 
     public void liftElevator() {
