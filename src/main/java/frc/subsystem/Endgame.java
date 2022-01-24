ca24a243f56e0bf53ab27765c6a782342b046fac
beasticle <julian.heidt@gmail.com>
2022-01-24 17:39:40 -0500

    final push verifing everything is correct

--- src/main/java/frc/subsystem/Endgame.java
+++ src/main/java/frc/subsystem/Endgame.java
@@ -20,14 +20,8 @@ public class Endgame {
     private final int ELEVATOR_PORT = 0;
     private final int ROTATOR_PORT = 0;
 
-<<<<<<< HEAD
     // Initalize objects
-    final CANSpark1038 rotator = new CANSpark1038(ELEVATOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
-    final RelativeEncoder rotateEncoder = rotator.getEncoder();
-=======
-    //Initalize objects
     final TalonFX1038 rotator = new TalonFX1038(ROTATOR_PORT);
->>>>>>> master
     final TalonFX1038 elevator = new TalonFX1038(ROTATOR_PORT);
 
     // Encoders for the Motors
@@ -36,11 +30,7 @@ public class Endgame {
     }
 
     public double getRotatorEncodePosition() {
-<<<<<<< HEAD
-        return rotateEncoder.getPosition(); // might need some extra weird math
-=======
-        return rotator.getPosition(); //might need some extra weird math
->>>>>>> master
+        return rotator.getPosition(); // might need some extra weird math
     }
 
     public void liftElevator() {
