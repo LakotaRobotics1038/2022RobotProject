4c340f9a61e261bb8c5af418fff99b387178b90b
Julian Heidt <Julian.heidt@gmail.com>
2022-02-17 17:39:13 -0500

    fixed some slight errors after fixing commit issues

--- src/main/java/frc/subsystem/Acquisition.java
+++ src/main/java/frc/subsystem/Acquisition.java
@@ -4,11 +4,14 @@ import frc.libraries.*;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Robot;
+import frc.libraries.Joystick1038;
 
 public class Acquisition implements Subsystem {
     private static Acquisition acquisition;
@@ -23,24 +26,25 @@ public class Acquisition implements Subsystem {
     // Motor ports *CHANGE THESE OR ROBOT GETS ANGRY!
 
     // Ports
-    private final int beaterBarPort = 0;
+    private final int spinnyBarPort = 0;
     // Solenoid channels
     private final int PUSH_OUT_ACQUISITION_CHANNEL = 0;
     private final int PULL_IN_ACQUISITION_CHANNEL = 0;
-    final CANSpark1038 beaterBar = new CANSpark1038(beaterBarPort, CANSparkMaxLowLevel.MotorType.kBrushless);
-    // Initializing the
+    private boolean AcquisitionIsIn = true;
+    final TalonFX1038 spinnyBar = new TalonFX1038(spinnyBarPort);
     private DoubleSolenoid acquisitionSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
             PUSH_OUT_ACQUISITION_CHANNEL, PULL_IN_ACQUISITION_CHANNEL);
     public AcquisitionStates acquisitionState = AcquisitionStates.In;
     public boolean XIsPressed = false;
 
+    // private final Joystick1038 joystick = Joystick1038.getInstance(0);
     public enum AcquisitionStates {
         In, Out
     }
 
     // Encoder
     // github is dumb
-    public RelativeEncoder motor1Encoder = beaterBar.getEncoder();
+    public double motor1Encoder = spinnyBar.getPosition();
     // Motor Speeds
     private final static double BEATER_BAR_SPEED = 0.65;
 
@@ -60,27 +64,31 @@ public class Acquisition implements Subsystem {
     }
 
     public void toggleAcqPos() {
-        if (XIsPressed) {
-            acquisitionState = AcquisitionStates.In;
-            XIsPressed = false;
-        }
-
-        else {
+        if ((AcquisitionIsIn)
+                && ((acquisitionSolenoid.get() != Value.kForward) || (acquisitionSolenoid.get() != Value.kReverse))) {
             acquisitionState = AcquisitionStates.Out;
-            XIsPressed = true;
+            AcquisitionIsIn = false;
+
+        }
+
+        else if ((!AcquisitionIsIn)
+                && ((acquisitionSolenoid.get() != Value.kForward) || (acquisitionSolenoid.get() != Value.kReverse))) {
+            acquisitionState = AcquisitionStates.In;
+            AcquisitionIsIn = true;
         }
 
     }
 
-    public void runBeaterBarFwd() {
-        beaterBar.set(BEATER_BAR_SPEED);
+    public void runspinnyBarFwd() {
+        // If motor is not moving
+        spinnyBar.set(BEATER_BAR_SPEED);
     }
 
-    public void runBeaterBarRev() {
-        beaterBar.set(-BEATER_BAR_SPEED);
+    public void runspinnyBarRev() {
+        spinnyBar.set(-BEATER_BAR_SPEED);
     }
 
-    public void stopBeaterBar() {
-        beaterBar.set(0);
+    public void stopspinnyBar() {
+        spinnyBar.set(0);
     }
 }
