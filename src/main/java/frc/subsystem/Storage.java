2b39333c160226c26404ccd9102f3c4652340c6c
Caleb Kingan <crkingan@gmail.com>
2022-03-18 19:16:34 -0400

    Changed storage code from 3 lasers to 2 lasers.

--- src/main/java/frc/subsystem/Storage.java
+++ src/main/java/frc/subsystem/Storage.java
@@ -11,7 +11,6 @@ public class Storage implements Subsystem {
     // Ports and Constants
     private final int SHUTTLE_MOTOR_PORT = 17;
     private final int START_LASER_PORT = 0;
-    private final int MID_LASER_PORT = 1;
     private final int END_LASER_PORT = 2;
     private final int SHUTTLE_MOTOR_ENCODER_COUNTS = 47;
     private final int ENCODER_OFFSET = 500;
@@ -24,7 +23,6 @@ public class Storage implements Subsystem {
     private CANSparkMax shuttleMotor = new CANSparkMax(SHUTTLE_MOTOR_PORT, MotorType.kBrushless);
     private RelativeEncoder shuttleMotorEncoder = shuttleMotor.getEncoder();
     private DigitalInput laserStart = new DigitalInput(START_LASER_PORT);
-    private DigitalInput laserMid = new DigitalInput(MID_LASER_PORT);
     private DigitalInput laserEnd = new DigitalInput(END_LASER_PORT);
 
     // manual drive
@@ -77,19 +75,10 @@ public class Storage implements Subsystem {
         switch (selectedManualStorageMode) {
             case None:
                 // If the ball is at the first laser and not the middle laser. Move the ball.
-                if (laserStart.get() && !laserMid.get()) {
+                if (laserStart.get() && !laserEnd.get()) {
                     shuttleMotor.set(shuttleMotorSpeed);
                     // If a ball is not at the first laser and its at the middle laser. Stop it.
-                } else if (!laserStart.get() && laserMid.get()) {
-                    shuttleMotor.set(0);
-                }
-                // If there is a ball at the first laser and the middle laser and not the final
-                // laser.
-                // Move the ball at the middle to the end and the one at the start to the end.
-                if (laserStart.get() && laserMid.get() && !laserEnd.get()) {
-                    shuttleMotor.set(shuttleMotorSpeed);
-                    // if the balls moved to their correct position then stop the belt.
-                } else if (!laserStart.get() && laserMid.get() && laserEnd.get()) {
+                } else if (!laserStart.get() && laserEnd.get()) {
                     shuttleMotor.set(0);
                 }
                 break;
