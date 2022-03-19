12a88c1c6b4d1182d9210fd8bb530ba1e54af551
Caleb Kingan <crkingan@gmail.com>
2022-03-19 13:44:36 -0400

    Created/debugged Commands for Auton

---
+++ src/main/java/frc/auton/Commands/ShootCommand.java
@@ -0,0 +1,36 @@
+package frc.auton.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Timer;
+import frc.subsystem.Shooter;
+import frc.subsystem.Storage;
+import frc.subsystem.Storage.ManualStorageModes;
+
+public class ShootCommand extends CommandBase {
+    private Shooter shooter = Shooter.getInstance();
+    private Storage storage = Storage.getInstance();
+
+    private final double END_TIME;
+
+    public ShootCommand(double endTime) {
+        END_TIME = endTime;
+    }
+
+    @Override
+    public void execute() {
+        shooter.executeSpeedPID();
+        storage.setManualStorage(ManualStorageModes.In);
+        storage.periodic();
+    }
+
+    @Override
+    public void end(boolean interuppted) {
+        shooter.shootManually(0);
+        shooter.noFeedBall();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Timer.getMatchTime() <= END_TIME;
+    }
+}
