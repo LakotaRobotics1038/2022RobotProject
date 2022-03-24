d2356c4f7a8df6d7fac661910b04b66c82b58f27
Caleb Kingan <crkingan@gmail.com>
2022-03-24 06:53:29 -0400

    Fixed Pathweaver path thingy

--- src/main/java/frc/auton/AutonSelector.java
+++ src/main/java/frc/auton/AutonSelector.java
@@ -3,12 +3,15 @@ package frc.auton;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import javax.swing.plaf.basic.BasicSplitPaneUI.BasicHorizontalLayoutManager;
+
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.auton.commands.AcquireCommand;
+import frc.auton.commands.AimCommand;
 import frc.auton.commands.DriveStraightCommand;
 import frc.auton.commands.ShootCommand;
 import frc.auton.*;
@@ -16,13 +19,14 @@ import frc.libraries.Dashboard;
 
 public class AutonSelector {
     // Path Options
-    public static final String TestPath = "TestPath";
+    public static final String AcquireAndShoot = "AcquireAndShoot";
     public static final String DriveStraight = "DriveStraight";
     public static final String DriveAndShoot = "DriveAndShoot";
     public static final String DriveShootandAcquire = "DriveShootandAcquire";
+    public static Boolean TheTryWorked = false;
 
     // Path Locations
-    private final String trajectoryJSON = "frc/auton/Pathweaver/Testpath.wpilib.json";
+    private final String trajectoryJSON = "frc/auton/Pathweaver/AcquireAndShoot.wpilib.json";
 
     // Fields
     private static AutonSelector autonSelector;
@@ -47,10 +51,11 @@ public class AutonSelector {
         System.out.println("auto: " + autonChooser);
 
         switch (autonChooser) {
-            case TestPath:
+            case AcquireAndShoot:
                 Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
                 try {
                     trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+                    new SequentialCommandGroup(new AcquireCommand(9.0), new AimCommand(9), new ShootCommand(9));
                 } catch (IOException ex) {
                     DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON,
                             ex.getStackTrace());
