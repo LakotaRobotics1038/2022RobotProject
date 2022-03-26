d7c799d030c7a3f5d3e2b17d09fda17ab6a70a8c
Wesley Reed <reed.wesley.s@gmail.com>
2022-03-26 09:54:23 -0400

    fixed turn command

--- src/main/java/frc/auton/Commands/TurnCommand.java
+++ src/main/java/frc/auton/Commands/TurnCommand.java
@@ -1,16 +1,15 @@
 package frc.auton.commands;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.libraries.DriveTrain1038;
 import frc.libraries.Gyro1038;
 
-public class TurnCommand extends CommandBase {
+public class TurnCommand extends PIDCommand {
     private static Gyro1038 gyroSensor = Gyro1038.getInstance();
-    private final DriveTrain1038 drive = DriveTrain1038.getInstance();
-
-    // private double drivePower = 0.0;
+    private static DriveTrain1038 drive = DriveTrain1038.getInstance();
     private final double TOLERANCE = 5.0;
     private final static double P = 0.035;
     private final static double I = 0.0015;
@@ -18,19 +17,29 @@ public class TurnCommand extends CommandBase {
 
     private PIDController turnPID;
 
-    public TurnCommand(double setpoint) {
-        turnPID = new PIDController(P, I, D);
+    /**
+     * Turns to robot to the specified angle.
+     *
+     * @param targetAngleDegrees The angle to turn to
+     */
+    public TurnCommand(double targetAngleDegrees) {
+        super(new PIDController(P, I, D),
+                gyroSensor::getAngle,
+                targetAngleDegrees,
+                output -> drive.arcadeDrive(0, output),
+                drive);
 
-        turnPID.setPID(P, I, D);
+        turnPID = getController();
 
-        turnPID.setSetpoint(setpoint);
-        System.out.println("setpoint" + turnPID.getSetpoint());
-        turnPID.disableContinuousInput();
-        // SmartDashboard.putData("Controls/turn", turnPID);
-
-        // Angle
-        turnPID.setTolerance(TOLERANCE);
         turnPID.enableContinuousInput(0, 360);
-        addRequirements(drive);
+        turnPID.setTolerance(TOLERANCE);
+
+        Shuffleboard.getTab("Controls").add("Turn Command PID", turnPID)
+                .withWidget(BuiltInWidgets.kPIDCommand);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return turnPID.atSetpoint();
     }
 }
