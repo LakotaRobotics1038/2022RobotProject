542d23b43ee449215d31ca7440165e1ba469fd5a
Wesley Reed <reed.wesley.s@gmail.com>
2022-03-30 18:38:39 -0400

    dumb talons

--- src/main/java/frc/libraries/TalonFX1038.java
+++ src/main/java/frc/libraries/TalonFX1038.java
@@ -1,5 +1,6 @@
 package frc.libraries;
 
+import com.ctre.phoenix.motorcontrol.StatusFrame;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
@@ -7,6 +8,14 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 public class TalonFX1038 extends TalonFX implements MotorController {
     public TalonFX1038(int address) {
         super(address);
+        this.setStatusFramePeriod(StatusFrame.Status_4_AinTempVbat, 255);
+        this.setStatusFramePeriod(StatusFrame.Status_6_Misc, 255);
+        this.setStatusFramePeriod(StatusFrame.Status_9_MotProfBuffer, 255);
+        this.setStatusFramePeriod(StatusFrame.Status_10_Targets, 255);
+        this.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 255);
+        this.setStatusFramePeriod(StatusFrame.Status_14_Turn_PIDF1, 255);
+        this.setStatusFramePeriod(StatusFrame.Status_15_FirmwareApiStatus, 255);
+        this.setStatusFramePeriod(StatusFrame.Status_17_Targets1, 255);
     }
 
     public void pidWrite(double output) {
