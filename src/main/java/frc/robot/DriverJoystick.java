b9fdf6017cb2207490778513e9a6f5c4f2fc89bd
Wesley Reed <reed.wesley.s@gmail.com>
2022-05-28 22:22:57 -0400

    trying out the slew rate limiter instead of multiplier

--- src/main/java/frc/robot/DriverJoystick.java
+++ src/main/java/frc/robot/DriverJoystick.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.DriveTrain.GearStates;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.commands.SetDriveGearStateCommand;
 import frc.robot.commands.ToggleDriveModeCommand;
@@ -21,33 +22,24 @@ public class DriverJoystick {
     public Joystick1038 driverJoystick = new Joystick1038(0);
     private final DriveTrain driveTrain = DriveTrain.getInstance();
 
-    private double drivePower = 0;
-    private double multiplier = .5;
-
     private DriverJoystick() {
+        SlewRateLimiter filter = new SlewRateLimiter(0.5);
+
         driveTrain.setDefaultCommand(
                 new RunCommand(
                         () -> {
                             switch (driveTrain.currentDriveMode) {
                                 case tankDrive:
-                                    driveTrain.tankDrive(driverJoystick.getLeftY() * multiplier,
-                                            driverJoystick.getRightY() * multiplier);
+                                    driveTrain.tankDrive(filter.calculate(driverJoystick.getLeftY()),
+                                            driverJoystick.getRightY());
                                     break;
                                 case dualArcadeDrive:
-                                    if (driverJoystick.getLeftY() > 0) {
-                                        // TODO: What is this extra math for?
-                                        drivePower = (driverJoystick.getLeftY() - .1) * (1 / .9);
-                                    } else if (driverJoystick.getLeftY() < 0) {
-                                        drivePower = (driverJoystick.getLeftY() + .1) * (1 / .9);
-                                    } else {
-                                        drivePower = 0;
-                                    }
-                                    driveTrain.arcadeDrive(drivePower * multiplier,
-                                            driverJoystick.getRightX() * multiplier);
+                                    driveTrain.arcadeDrive(filter.calculate(driverJoystick.getLeftY()),
+                                            driverJoystick.getRightX());
                                     break;
                                 case singleArcadeDrive:
-                                    driveTrain.arcadeDrive(driverJoystick.getLeftY() * multiplier,
-                                            driverJoystick.getLeftX() * multiplier);
+                                    driveTrain.arcadeDrive(filter.calculate(driverJoystick.getLeftY()),
+                                            driverJoystick.getLeftX());
                                     break;
                             }
                         },
@@ -56,12 +48,5 @@ public class DriverJoystick {
         driverJoystick.backButton.whenPressed(new ToggleDriveModeCommand());
         driverJoystick.rightBumper.whenPressed(new SetDriveGearStateCommand(GearStates.High));
         driverJoystick.rightBumper.whenReleased(new SetDriveGearStateCommand(GearStates.Low));
-        driverJoystick.rightTrigger
-                .and(driverJoystick.rightBumper.negate())
-                .whenActive(() -> multiplier = 0.8);
-        driverJoystick.rightBumper.whenPressed(() -> multiplier = 1.0);
-        driverJoystick.rightBumper
-                .and(driverJoystick.rightTrigger)
-                .whenInactive(() -> multiplier = 0.5);
     }
 }
