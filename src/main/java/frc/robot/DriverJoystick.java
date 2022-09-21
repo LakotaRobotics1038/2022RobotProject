f4652043b2108d37dd4c91f9bfc067ce0976bb74
Wesley Reed <reed.wesley.s@gmail.com>
2022-09-21 19:47:11 -0400

    better slew limiting

--- src/main/java/frc/robot/DriverJoystick.java
+++ src/main/java/frc/robot/DriverJoystick.java
@@ -21,23 +21,32 @@ public class DriverJoystick {
 
     public Joystick1038 driverJoystick = new Joystick1038(0);
     private final DriveTrain driveTrain = DriveTrain.getInstance();
+    private double prevLeftY = 0;
+    private double prevRightX = 0;
 
     private DriverJoystick() {
-        SlewRateLimiter driveFilter = new SlewRateLimiter(0.7);
+        SlewRateLimiter driveFilter = new SlewRateLimiter(1.0);
+        SlewRateLimiter turnFilter = new SlewRateLimiter(1.0);
 
         driveTrain.setDefaultCommand(
                 new RunCommand(
                         () -> {
                             double leftY = driverJoystick.getLeftY();
-                            if (leftY == 0) {
+                            if (signChange(leftY, prevLeftY)) {
                                 driveFilter.reset(0);
                             }
                             double limitedLeftY = driveFilter.calculate(leftY);
 
+                            double rightX = driverJoystick.getRightX();
+                            if (signChange(rightX, prevRightX)) {
+                                turnFilter.reset(0);
+                            }
+                            double limitedRightX = turnFilter.calculate(rightX);
+
                             switch (driveTrain.currentDriveMode) {
                                 case tankDrive:
                                     driveTrain.tankDrive(limitedLeftY,
-                                            driverJoystick.getRightY());
+                                            limitedRightX);
                                     break;
                                 case dualArcadeDrive:
                                     driveTrain.arcadeDrive(limitedLeftY,
@@ -48,11 +57,17 @@ public class DriverJoystick {
                                             driverJoystick.getLeftX());
                                     break;
                             }
+                            prevLeftY = leftY;
+                            prevRightX = rightX;
                         },
                         driveTrain));
 
         driverJoystick.backButton.whenPressed(new ToggleDriveModeCommand());
         driverJoystick.rightBumper.whenPressed(new SetDriveGearStateCommand(GearStates.High));
         driverJoystick.rightBumper.whenReleased(new SetDriveGearStateCommand(GearStates.Low));
+    }
+
+    private boolean signChange(double a, double b) {
+        return a > 0 && b < 0 || b > 0 && a < 0;
     }
 }
